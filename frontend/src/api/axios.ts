6e2d4bfb132faeacfaafc367b84b1a6f3fe008b6
rakesh <rakesh.s@skillsigma.com>
2026-01-23 12:39:30 +0530

    refresh token setting

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,23 +1,60 @@
 // src/api/axios.ts
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL;
+
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: baseURL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Request interceptor → attach access token
 API.interceptors.request.use(
   (config) => {
     const access = localStorage.getItem("access");
-    if (access && config.headers) {
+    if (access) {
       config.headers.Authorization = `Bearer ${access}`;
     }
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+API.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const refreshToken = localStorage.getItem("refresh");
+        if (!refreshToken) {
+            throw new Error("No refresh token stored");
+        }
+        const response = await axios.post(`${baseURL}/auth/token/refresh/`, {
+          refresh: refreshToken,
+        });
+
+        const newAccessToken = response.data.access;
+        localStorage.setItem("access", newAccessToken);
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return API(originalRequest);
+
+      } catch (refreshError) {
+        console.error("Session expired. logging out...");
+        
+        localStorage.removeItem("access");
+        localStorage.removeItem("refresh");
+        window.location.href = "/login";
+        
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
 );
 
 export default API;
